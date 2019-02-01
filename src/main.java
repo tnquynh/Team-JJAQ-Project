253dfe7729fdde852d95480f7f2c4b65da7b442a
jmeolmsted <jmeolmsted@users.noreply.github.com>
2019-01-31 19:54:39 -0800

    test

--- src/main.java
+++ src/main.java
@@ -1,1 +1,13 @@
 
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+
+	}
+
+}
