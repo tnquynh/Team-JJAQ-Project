6f3e4c88dca7bce4f23bfb7b91f4c916e0678160
jmeolmsted <jmeolmsted@users.noreply.github.com>
2019-01-31 19:48:07 -0800

    Update main.java

--- src/main.java
+++ src/main.java
@@ -3,8 +3,8 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Test")
-		System.out.println("Test")
+		System.out.println("Test");
+		System.out.println("Test");
 
 	}
 
