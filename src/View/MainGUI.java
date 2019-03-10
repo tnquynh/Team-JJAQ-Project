4acbb377dc523fda4f4f1b24457d80c6489354d1
jdick <jdick@146.187.208.44>
2019-03-09 16:31:50 -0800

    Home screen

--- src/View/MainGUI.java
+++ src/View/MainGUI.java
@@ -5,13 +5,20 @@
 package View;
 
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -25,6 +32,9 @@ public class MainGUI {
 	private VersionInfo myVersion;
 	private static final Dimension SIZE = new Dimension(450, 450);
 	private JFrame myFrame;
+	private JPanel myMainPanel;
+	private JPanel myNewPPanel;
+
 
 	/**
 	 * Is the main constructor for the GUI.
@@ -32,13 +42,15 @@ public class MainGUI {
 	public MainGUI() {
 		myVersion = new VersionInfo();
 		myFrame = new JFrame("JJAQ Program");
-
+		myMainPanel = new JPanel(new GridLayout(0, 3));
+		myNewPPanel = new JPanel(new GridLayout(0, 3));
 	}
 
 	/**
 	 * Starts the GUI.
 	 */
 	public void start() {
+		makeNewProjectPanel();
 		makePanel();
 		myFrame.setPreferredSize(SIZE);
 
@@ -55,9 +67,71 @@ public class MainGUI {
 	 * Makes the panel.
 	 */
 	private void makePanel() {
+		JPanel buttonPanel = new JPanel(new GridLayout(3, 0, 3, 10));
+		JButton newP = new JButton("New Project");
+		JButton oldP = new JButton("Open Project");
+		JButton compareP = new JButton("Compare");
+		newP.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) { 
+			    goToNew();
+			    myFrame.revalidate();
+				myFrame.repaint();
 
+			  } 
+			} );
+		buttonPanel.add(newP);
+		buttonPanel.add(oldP);
+		buttonPanel.add(compareP);
+		myMainPanel.add(new JPanel());
+		myMainPanel.add(new JPanel());
+		myMainPanel.add(new JPanel());
+		myMainPanel.add(new JPanel());
+
+
+
+
+		myMainPanel.add(buttonPanel);
+		myMainPanel.add(new JPanel());
+
+		myMainPanel.add(new JPanel());
+
+		myFrame.add(myMainPanel);
 	}
 
+	 private void goToNew() {
+		 myFrame.remove(myMainPanel);
+		 myFrame.add(myNewPPanel);
+		 System.out.println("Test");
+	 }
+	 
+	 private void makeNewProjectPanel() {
+			JPanel inputPanel = new JPanel();
+			JPanel buttonPanel = new JPanel(new GridLayout(3, 0, 3, 10));
+			JButton makeP = new JButton("Make Project");
+			JButton cancelP = new JButton("Cancel");
+			JTextField newP = new JTextField(5);
+			inputPanel.add(new JLabel("New Project:"));
+			inputPanel.add(newP);
+			buttonPanel.add(makeP);
+			buttonPanel.add(cancelP);
+			JButton home = new JButton("Home");
+
+			myNewPPanel.add(new JPanel());
+			myNewPPanel.add(new JPanel());
+			myNewPPanel.add(new JPanel());
+			myNewPPanel.add(new JPanel());
+
+
+
+
+			myNewPPanel.add(inputPanel);
+			myNewPPanel.add(new JPanel());
+			myNewPPanel.add(new JPanel());
+
+			myNewPPanel.add(buttonPanel);
+
+	 }
+	
 	/**
 	 * Creates a menu bar.
 	 * 
